04892dbf0a49710b83e7f14a331bd5a4b1d672a0
Douglas Binder <binder@app.fintech.io>
2015-05-18 17:40:22 -0400

    event emitter for the win

---
+++ dispatcher.js
@@ -0,0 +1,195 @@
+var EE = require("events").EventEmitter,
+    async = require("async");
+
+var config = require("./config.js");
+
+var MC_FINTECH_LIVE=config.list_id || process.env.list_id;
+var MailChimpAPI = require('mailchimp').MailChimpAPI
+var apikey = config.mailchimp_api || process.env.m_api;
+
+var dispatcher = new EE()
+
+var authorized_emails = [
+    "brooks@fintech.io",
+    "gareth@fintech.io",
+    "travis@fintech.io",
+    "doug@fintech.io"
+]
+var _u = config.username;
+var api;
+    try {
+         api = new MailChimpAPI(apikey, {
+            version: '2.0'
+        });
+    } catch (error) {
+        //console.log(error.message);
+    }
+
+
+dispatcher.on("newEmail", function(email, responder){
+    console.log(email);
+
+    var _admin = email.from[0];
+    if (authorized_emails.indexOf(_admin.address.toLowerCase()) > -1){
+       // console.log("AUTHORAIZED USER")
+    
+
+    if(email.to){
+        if (email.to.some(function(curr){return curr.address === _u})){
+            dispatcher.emit('directAdd', email, responder)};
+    }
+
+    if (email.cc){
+        if (email.cc.some(function(curr){ return curr.address === _u})){
+            dispatcher.emit('CC', email, responder)};
+    }
+
+    } else {
+        dispatcher.emit("unathorized");
+    }
+
+
+})
+
+dispatcher.on('directAdd', function(email, responder){
+    var _subscribers = email.text.split("\n").map(function(curr){
+            return {
+                 name: curr.split(' ').splice(-1, 1).join(' '),
+                 address: curr.split(' ').slice(-1).join(' ')
+            }}).filter(function(curr){
+                return curr.name !== '' && curr.address !== '' /*cleans empty lines*/
+                });
+            dispatcher.emit("addSubscribers", email, responder, _subscribers);
+})
+
+
+
+dispatcher.on("addSubscribers", function(email, responder, subscribers){
+    var _text = "";
+    async.each(subscribers, function(sub, callback){
+        var _sub = sub;
+        //console.log(sub)
+        api.call("lists", "subscribe", {
+                apikey:apikey,
+                id: MC_FINTECH_LIVE,
+                double_optin: false,
+                send_welcome:true,
+                email: {email: sub.address,
+                merge_vars:{
+                    NAME: sub["name"],
+                    NEWS: "Weekly Round Up" 
+                }
+            }}, function(err){
+                //console.log(_text)
+                if (err) {
+                    console.log(err);
+                    var _t = ""
+                    if (sub.name) {_t = _sub.name + ' ' +  err.message}
+                            else {_t = _sub.address + ' ' + err.message}
+                    _text += _t
+                    //console.log("TEXT:" +_text);
+                    callback(err);
+                    }else{
+                        var _t; 
+                        if (sub.name) {_t = _sub.name + " was added to the FinTech Live Mailinglist\n"}
+                            else{_t = _sub.address + " was added to the FinTech Live Mailinglist\n"}
+                        //console.log(_sub.name)
+                        _text += _t;
+                       // console.log("TEXT:" +_text)
+                callback();
+                }
+            }); 
+                },function(err){
+                    if (err){ 
+                        console.log(err);
+                        }
+       // console.log(_text);
+        responder.sendMail({
+            subject:email.subject,
+            from: _u,
+            to: email.from[0].address,
+            inReplyTo:email.messageId,
+            references:[email.messageId],
+            text: _text
+
+        }, function(err, data){
+            if (err) console.log(err);
+            //console.log(data);    
+        })
+    });/*END CC'ed on an email chain*/
+
+
+})
+
+
+
+dispatcher.on('CC', function(email, responder){
+
+
+    var _subscribers = email.to.map(function(curr){
+        return {address: curr["address"],
+                name: curr["name"]
+                }
+    })
+
+    dispatcher.emit("addSubscribers", email, responder, _subscribers)
+
+    /* CC'ed on an email chain
+    async.each(_subscribers, function(sub, callback){
+        var _sub = sub;
+        //console.log(sub)
+        api.call("lists", "subscribe", {
+                apikey:apikey,
+                id: MC_FINTECH_LIVE,
+                double_optin: false,
+                send_welcome:true,
+                email: {email: sub.address,
+                merge_vars:{
+                    NAME: sub["name"],
+                    NEWS: "Weekly Round Up" 
+                }
+            }}, function(err){
+                //console.log(_text)
+                if (err) {
+                    console.log(err);
+                    var _t = ""
+                    if (sub.name) {_t = _sub.name + ' ' +  err.message}
+                            else {_t = _sub.address + ' ' + err.message}
+                    _text += _t
+                    //console.log("TEXT:" +_text);
+                    callback(err);
+                    }else{
+                        var _t; 
+                        if (sub.name) {_t = _sub.name + " was added to the FinTech Live Mailinglist\n"}
+                            else{_t = _sub.address + " was added to the FinTech Live Mailinglist\n"}
+                        //console.log(_sub.name)
+                        _text += _t;
+                       // console.log("TEXT:" +_text)
+                callback();
+                }
+            }); 
+                },function(err){
+                    if (err){ 
+                        console.log(err);
+                        }
+       // console.log(_text);
+        responder.sendMail({
+            subject:email.subject,
+            from: config.username,
+            to: _u,
+            inReplyTo:email.messageId,
+            references:[email.messageId],
+            text: _text
+
+        }, function(err, data){
+            if (err) console.log(err);
+            //console.log(data);    
+        })
+    });END CC'ed on an email chain*/
+
+})
+
+dispatcher.on('response', function(){})
+dispatcher.on("unauthorized", function(){})
+
+module.exports  = dispatcher;
--- server.js
+++ server.js
@@ -1,35 +1,15 @@
 var config = require("./config.js");
 var Imap = require("imap"),
-util = require("util"),
-inspect = require('util').inspect,
-MailParser = require("mailparser").MailParser,
-mailParser = new MailParser(),
-nodemailer = require("nodemailer"),
-async = require("async");
+    util = require("util"),
+    inspect = require('util').inspect,
+    MailParser = require("mailparser").MailParser,
+    mailParser = new MailParser(),
+    nodemailer = require("nodemailer"),
+    dispatcher = require("./dispatcher");
+
 
 email = config.username || process.env.username;
 password = config.password ||process.env.password;
-
-var apikey = config.mailchimp_api || process.env.m_api;
-var MC_FINTECH_LIVE=config.list_id || process.env.list_id;
-
-var MailChimpAPI = require('mailchimp').MailChimpAPI
-var api;
-    try {
-         api = new MailChimpAPI(apikey, {
-            version: '2.0'
-        });
-    } catch (error) {
-        //console.log(error.message);
-    }
-
-var authorized_emails = [
-    "brooks@fintech.io",
-    "gareth@fintech.io",
-    "travis@fintech.io",
-    "doug@fintech.io"
-]
-
 
 
 
@@ -51,89 +31,15 @@ var imap = new Imap({
   tls: true
 });
 
-
 function openInbox(cb) {
   imap.openBox('INBOX', false, cb);
 }
 
-
 /* PARSES RAW TEXT EMAILS TO JSON */
 mailParser.on("end", function(email){
-    //console.log(email);
-    var admin = email.from[0];
     console.log("=====================================");
+    dispatcher.emit("newEmail", email, responder);
     //console.log(email);
-
-    var _text = "";
-
-    if (authorized_emails.indexOf(admin.address.toLowerCase()) > -1){
-       // console.log("AUTHORAIZED USER")
-    var _subscribers = email.to.map(function(curr){
-        return {address: curr["address"],
-                name: curr["name"]
-                }
-    })
- 
-    async.each(_subscribers, function(sub, callback){
-        var _sub = sub;
-        //console.log(sub)
-        api.call("lists", "subscribe", {
-                apikey:apikey,
-                id: MC_FINTECH_LIVE,
-                double_optin: false,
-                send_welcome:true,
-                email: {email: sub.address,
-                merge_vars:{
-                    NAME: sub["name"],
-                    NEWS: "Weekly Round Up" 
-                }
-            }}, function(err){
-                //console.log(_text)
-                if (err) {
-                    console.log(err);
-                    var _t = ""
-                    if (sub.name) {_t = _sub.name + ' ' +  err.message}
-                            else {_t = _sub.address + ' ' + err.message}
-                    _text += _t
-                    //console.log("TEXT:" +_text);
-                    callback(err);
-                    }else{
-                        var _t; 
-                        if (sub.name) {_t = _sub.name + " was added to the FinTech Live Mailinglist\n"}
-                            else{_t = _sub.address + " was added to the FinTech Live Mailinglist\n"}
-                        //console.log(_sub.name)
-                        _text += _t;
-                       // console.log("TEXT:" +_text)
-                callback();
-                }
-            }); 
-                },function(err){
-                    if (err){ 
-                        console.log(err);
-                        }
-
-       // console.log(_text);
-        
-        responder.sendMail({
-            subject:email.subject,
-            from: config.username,
-            to: email.from[0].address,
-            inReplyTo:email.messageId,
-            references:[email.messageId],
-            text: _text
-
-        }, function(err, data){
-            if (err) console.log(err);
-            //console.log(data);    
-        })
-
-
-
-
-
-                    });
-                }
-                
 
 })
 
