75c84fa0b4074fc1b28b21691b48aaf545723627
Douglas Binder <binder@app.fintech.io>
2015-05-18 14:33:00 -0400

    added config to repository

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var config = require("config.js");
+var config = require("./config.js");
 var Imap = require("imap"),
 inspect = require('util').inspect,
 MailParser = require("mailparser").MailParser,
